8be2269f48f0a61636efad374a0ceafe2e81207a
dzshao <danielzhshao@gmail.com>
2023-05-25 23:52:49 -0700

    Created ScrapeWebsite class and implemented scrapeSite function

---
+++ header/ScrapeWebsite.h
@@ -0,0 +1,15 @@
+#ifndef SCRAPE_WEBSITE
+#define SCRAPE_WEBSITE
+
+#include <string>
+#include <iostream>
+
+using std::cout;
+using std::string;
+
+class ScrapeWebsite {
+    public:
+        static string & scrapeSite(const string &) 
+};
+
+#endif
---
+++ src/ScrapeWebsite.cpp
@@ -0,0 +1,44 @@
+#include "../header/ScrapeWebsite.h"
+#include <curl/curl.h>
+
+/* This program uses cURL, which will not work on the CS100 server. For this to run, you first need to install cURL
+on your computer by running: "sudo apt-get install curl" in the terminal. 
+When you compile this program, make sure to add the -lcurl flag. Ex. "g++ ... -lcurl"
+*/
+size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata)
+{
+    // This function is called by libcurl when data is received from the website
+    // You can process the received data here
+    string& text = *static_cast<string*>(userdata);
+    size_t totalsize = size * nmemb;
+    text.append(static_cast<char*>(ptr), totalsize);
+
+    return totalsize;
+}
+
+static string & ScrapeWebsite::scrapeSite(const string &websiteURL) {
+    CURL *websiteConnection = nullptr;
+    CURLcode res;
+    string siteHTML;
+    
+    websiteConnection = curl_easy_init();
+    if(websiteConnection)
+    {   
+        // Set the URL to be scraped
+        curl_easy_setopt(websiteConnection, CURLOPT_URL, websiteURL.c_str());
+
+        // Set the callback function to be called by libcurl when data is received (write_callback function defined above)
+        curl_easy_setopt(websiteConnection, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(websiteConnection, CURLOPT_WRITEDATA, &siteHTML);
+
+        // Scraping the website
+        res = curl_easy_perform(websiteConnection);
+        if(res != CURLE_OK) {
+            cout << "Error: " << curl_easy_strerror(res) << endl;
+        }
+
+        // Clean up
+        curl_easy_cleanup(curl);
+    }
+    return siteHTML;
+}
