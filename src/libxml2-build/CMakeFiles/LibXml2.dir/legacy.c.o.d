422c7c344534eaf736ea17a0a0fbf7ef610e96fc
dzshao <danielzhshao@gmail.com>
2023-05-25 23:39:51 -0700

    Removed libxml library

--- src/libxml2-build/CMakeFiles/LibXml2.dir/legacy.c.o.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/LibXml2.dir/legacy.c.o: \
- /home/csmajs/ahude001/final-project-ahude001-dshao009-jshen075-jrive141/src/libxml2/legacy.c \
- /usr/include/stdc-predef.h \
- /home/csmajs/ahude001/final-project-ahude001-dshao009-jshen075-jrive141/src/libxml2/libxml.h \
- /home/csmajs/ahude001/final-project-ahude001-dshao009-jshen075-jrive141/src/libxml2-build/config.h \
- /home/csmajs/ahude001/final-project-ahude001-dshao009-jshen075-jrive141/src/libxml2-build/libxml/xmlversion.h \
- /home/csmajs/ahude001/final-project-ahude001-dshao009-jshen075-jrive141/src/libxml2/include/libxml/xmlexports.h
--- src/libxml2-build/CMakeFiles/LibXml2.dir/xzlib.c.o.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/LibXml2.dir/xzlib.c.o: \
- /home/csmajs/ahude001/final-project-ahude001-dshao009-jshen075-jrive141/src/libxml2/xzlib.c \
- /usr/include/stdc-predef.h \
- /home/csmajs/ahude001/final-project-ahude001-dshao009-jshen075-jrive141/src/libxml2/libxml.h \
- /home/csmajs/ahude001/final-project-ahude001-dshao009-jshen075-jrive141/src/libxml2-build/config.h \
- /home/csmajs/ahude001/final-project-ahude001-dshao009-jshen075-jrive141/src/libxml2-build/libxml/xmlversion.h \
- /home/csmajs/ahude001/final-project-ahude001-dshao009-jshen075-jrive141/src/libxml2/include/libxml/xmlexports.h
