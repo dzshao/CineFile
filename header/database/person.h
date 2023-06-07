39a9374130d91701eb2d66f1bdd7213739b245f7
dzshao <danielzhshao@gmail.com>
2023-06-06 20:59:35 -0700

    Updated imdbParser tests to include release date tests

--- header/database/person.h
+++ header/database/person.h
@@ -2,12 +2,27 @@
 #define PERSON_H
 
 #include <vector>
+#include <string>
 
 class Person {
+    public:
+        Person();
+
+        Person(std::string _name) : 
+                name(_name)
+        {};
+
+        Person(std::string _name, const std::vector<std::string>&movieList) : 
+                name(_name), 
+                movies(movieList)
+        {};
+
+        std::vector<std::string>& getMovieList() const;
+        std::string getName() const;
+
     protected:
         std::vector<std::string> movies;
-    public:
-        std::vector<std::string>& getMovieList();
+        std::string name;
 };
 
 #endif
--- tests/imdbParser_tests.h
+++ tests/imdbParser_tests.h
@@ -5,32 +5,61 @@
 
 #include "../header/webscraping/imdbParser.h"
 
+std::vector<Movie> horrorList{{"Yellowjackets", "(2021- )"}, 
+                                       {"From", "(2022- )"}, 
+                                       {"The Boogeyman", "(2023)"}, 
+                                       {"The Walking Dead", "(2010-2022)"}};
+
+std::vector<Movie> romanceList{{"The Little Mermaid", "(2023)", 7.2}, 
+                                        {"Barbie", "(2023)"}, 
+                                        {"Asteroid City", "(2023)"}, 
+                                        {"Queen Charlotte: A Brigerton Story", "(2023)"}};
+
 TEST(imdbParserTest, testHorror) {
     vector<Movie> listOfHorrorMovies = imdbParser::scrapeGenres({"horror"});
-    EXPECT_EQ(listOfHorrorMovies.at(0).name, "Yellowjackets");
-    EXPECT_EQ(listOfHorrorMovies.at(1).name, "From");
-    EXPECT_EQ(listOfHorrorMovies.at(2).name, "The Boogeyman");
-    EXPECT_EQ(listOfHorrorMovies.at(3).name, "The Walking Dead");
+    EXPECT_EQ(listOfHorrorMovies.at(0).name, horrorList.at(0).name);
+    EXPECT_EQ(listOfHorrorMovies.at(1).name, horrorList.at(1).name);
+    EXPECT_EQ(listOfHorrorMovies.at(2).name, horrorList.at(2).name);
+    EXPECT_EQ(listOfHorrorMovies.at(3).name, horrorList.at(3).name);
+
+    EXPECT_EQ(listOfHorrorMovies.at(0).releaseDates, horrorList.at(0).releaseDates);
+    EXPECT_EQ(listOfHorrorMovies.at(1).releaseDates, horrorList.at(1).releaseDates);
+    EXPECT_EQ(listOfHorrorMovies.at(2).releaseDates, horrorList.at(2).releaseDates);
+    EXPECT_EQ(listOfHorrorMovies.at(3).releaseDates, horrorList.at(3).releaseDates);
 }
 
 TEST(imdbParserTest, testRomance) {
     vector<Movie> listOfRomanceMovies = imdbParser::scrapeGenres({"romance"});
-    EXPECT_EQ(listOfRomanceMovies.at(0).name, "The Little Mermaid");
-    EXPECT_EQ(listOfRomanceMovies.at(1).name, "Barbie");
-    EXPECT_EQ(listOfRomanceMovies.at(2).name, "Asteroid City");
-    EXPECT_EQ(listOfRomanceMovies.at(3).name, "Queen Charlotte: A Bridgerton Story");
+    EXPECT_EQ(listOfRomanceMovies.at(0).name, romanceList.at(0).name);
+    EXPECT_EQ(listOfRomanceMovies.at(1).name, romanceList.at(1).name);
+    EXPECT_EQ(listOfRomanceMovies.at(2).name, romanceList.at(2).name);
+    EXPECT_EQ(listOfRomanceMovies.at(3).name, romanceList.at(3).name);
+
+    EXPECT_EQ(listOfRomanceMovies.at(0).releaseDates, romanceList.at(0).releaseDates);
+    EXPECT_EQ(listOfRomanceMovies.at(1).releaseDates, romanceList.at(1).releaseDates);
+    EXPECT_EQ(listOfRomanceMovies.at(2).releaseDates, romanceList.at(2).releaseDates);
+    EXPECT_EQ(listOfRomanceMovies.at(3).releaseDates, romanceList.at(3).releaseDates);
 }
 
 TEST(imdbParserTest, testTwoGenres) {
     vector<Movie> listOfHorrorRomanceMovies = imdbParser::scrapeGenres({"romance", "horror"});
-    EXPECT_EQ(listOfHorrorRomanceMovies.at(0).name, "The Little Mermaid");
-    EXPECT_EQ(listOfHorrorRomanceMovies.at(1).name, "Barbie");
-    EXPECT_EQ(listOfHorrorRomanceMovies.at(2).name, "Asteroid City");
-    EXPECT_EQ(listOfHorrorRomanceMovies.at(3).name, "Queen Charlotte: A Bridgerton Story");
-    EXPECT_EQ(listOfHorrorRomanceMovies.at(10).name, "Yellowjackets");
-    EXPECT_EQ(listOfHorrorRomanceMovies.at(11).name, "From");
-    EXPECT_EQ(listOfHorrorRomanceMovies.at(12).name, "The Boogeyman");
-    EXPECT_EQ(listOfHorrorRomanceMovies.at(13).name, "The Walking Dead");
+    // Checking if the romance movies parsed are correct
+    EXPECT_EQ(listOfHorrorRomanceMovies.at(0).name, romanceList.at(0).name);
+    EXPECT_EQ(listOfHorrorRomanceMovies.at(1).name, romanceList.at(1).name);
+    EXPECT_EQ(listOfHorrorRomanceMovies.at(2).name, romanceList.at(2).name);
+
+    EXPECT_EQ(listOfHorrorRomanceMovies.at(0).releaseDates, romanceList.at(0).releaseDates);
+    EXPECT_EQ(listOfHorrorRomanceMovies.at(1).releaseDates, romanceList.at(1).releaseDates);
+    EXPECT_EQ(listOfHorrorRomanceMovies.at(2).releaseDates, romanceList.at(2).releaseDates);
+
+     // Checking if the horror movies parsed are correct
+    EXPECT_EQ(listOfHorrorRomanceMovies.at(10).name, horrorList.at(0).name);
+    EXPECT_EQ(listOfHorrorRomanceMovies.at(11).name, horrorList.at(1).name);
+    EXPECT_EQ(listOfHorrorRomanceMovies.at(12).name, horrorList.at(2).name);
+
+    EXPECT_EQ(listOfHorrorRomanceMovies.at(10).releaseDates, horrorList.at(0).releaseDates);
+    EXPECT_EQ(listOfHorrorRomanceMovies.at(11).releaseDates, horrorList.at(1).releaseDates);
+    EXPECT_EQ(listOfHorrorRomanceMovies.at(12).releaseDates, horrorList.at(2).releaseDates);
 }
 
 TEST(imdbParserTest, testInvalidGenre) {
@@ -52,10 +81,16 @@ TEST(imdbParserTest, testOneInvalidGenreOneValid) {
     vector<Movie> listOfHorrorBogusMovies = imdbParser::scrapeGenres({"bogusGenre", "horror"});
     string output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "Please enter a valid genre. (bogusGenre is not a valid genre)\n");
-    EXPECT_EQ(listOfHorrorBogusMovies.at(0).name, "Yellowjackets");
-    EXPECT_EQ(listOfHorrorBogusMovies.at(1).name, "From");
-    EXPECT_EQ(listOfHorrorBogusMovies.at(2).name, "The Boogeyman");
-    EXPECT_EQ(listOfHorrorBogusMovies.at(3).name, "The Walking Dead");
+
+    EXPECT_EQ(listOfHorrorBogusMovies.at(0).name, horrorList.at(0).name);
+    EXPECT_EQ(listOfHorrorBogusMovies.at(1).name, horrorList.at(1).name);
+    EXPECT_EQ(listOfHorrorBogusMovies.at(2).name, horrorList.at(2).name);
+    EXPECT_EQ(listOfHorrorBogusMovies.at(3).name, horrorList.at(3).name);
+
+    EXPECT_EQ(listOfHorrorBogusMovies.at(0).releaseDates, horrorList.at(0).releaseDates);
+    EXPECT_EQ(listOfHorrorBogusMovies.at(1).releaseDates, horrorList.at(1).releaseDates);
+    EXPECT_EQ(listOfHorrorBogusMovies.at(2).releaseDates, horrorList.at(2).releaseDates);
+    EXPECT_EQ(listOfHorrorBogusMovies.at(3).releaseDates, horrorList.at(3).releaseDates);
 }
 
 #endif
