55c8fca84da80db2ce278cc783bdde58e15796ab
dzshao <danielzhshao@gmail.com>
2023-06-08 21:17:44 -0700

    Moved movie.cpp to the correct folder

--- src/database/movie.cpp
+++ src/database/movie.cpp
@@ -3,21 +3,31 @@
 
 using std::endl;
 
-std::ostream& operator<<(std::ostream& out, const Movie& outputMovie) {        
-    out << outputMovie.name << endl
-        << outputMovie.releaseDates << endl;
+std::ostream& operator<<(std::ostream& out, const Movie& outputMovie) {
+    out << outputMovie.name << " " << outputMovie.releaseDates << ":" << endl;
+
+    out << "Rating: ";
+    if (outputMovie.rating == 0.0) {
+        out << "N/A" << endl;
+    } 
+    else {
+        out << outputMovie.rating << "/10" << endl;
+    }
+
+    out << "Genre(s): ";
     for(unsigned i = 0; i < outputMovie.genreList.size() - 1; ++i) {
         out << outputMovie.genreList.at(i).getName() << ", ";
     }
     out << outputMovie.genreList.at(outputMovie.genreList.size() - 1).getName() << endl;
 
-    out << outputMovie.rating << endl;
-
+    out << "Directed by: ";
     for(unsigned i = 0; i < outputMovie.directorList.size() - 1; ++i) {
         out << outputMovie.directorList.at(i).getName() << ", ";
     }
     out << outputMovie.directorList.at(outputMovie.directorList.size() - 1).getName() << endl;   
 
+
+    out << "Starring: ";
     for(unsigned i = 0; i < outputMovie.castList.size() - 1; ++i) {
         out << outputMovie.castList.at(i).getName() << ", ";
     }
