696ddc869a311a6c2891fa9c91115ebff6d2ab21
jleans <enderblader@gmail.com>
2023-06-09 04:02:15 -0700

    tsa

--- tests/Recommend_tests.h
+++ tests/Recommend_tests.h
@@ -1,15 +1,17 @@
 #ifndef RECOMMEND_TESTS
 #define RECOMMEND_TESTS
 
+
+#include <vector>
 #include <gtest/gtest.h>
 #include "../header/database/MovieRec.h"
 
 TEST(Recommendtests, noerrors){
     Movie randomMovie{"Yellowjackets", "(2021– )", {{"Drama"}, {"Horror"}, {"Mystery"}}, 7.8, {{"N/A"}}, {{"Melanie Lynskey"}, {"Tawny Cypress"}, {"Sophie Nélisse"}, {"Jasmin Savoy Brown"}}};
     MoviesDatabase::addMovie(randomMovie);
-    vector<string> tactors = {"Bob", "Fred"};
-    vector<string> tdirect = {"John"};
-    vector<string> tgenre = {"Horror", "Comedy"};
+    std::vector<string> tactors = {"Bob", "Fred"};
+    std::vector<string> tdirect = {"John"};
+    std::vector<string> tgenre = {"Horror", "Comedy"};
     char tc = 'A';
     EXPECT_NO_THROW(MovieRec::recommend(tc, tgenre, tdirect, tactors));
 }
