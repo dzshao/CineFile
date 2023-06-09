d39cfbb4d83576c29f2c311568e8a44b4830570c
jleans <enderblader@gmail.com>
2023-06-09 03:52:41 -0700

    grrr

--- tests/Recommend_tests.h
+++ tests/Recommend_tests.h
@@ -8,7 +8,7 @@ TEST(Recommendtests, noerrors){
     Movie randomMovie{"Yellowjackets", "(2021– )", {{"Drama"}, {"Horror"}, {"Mystery"}}, 7.8, {{"N/A"}}, {{"Melanie Lynskey"}, {"Tawny Cypress"}, {"Sophie Nélisse"}, {"Jasmin Savoy Brown"}}};
     MoviesDatabase::addMovie(randomMovie);
     vector<string> tactors = {"Bob", "Fred"};
-    vector<string tdirect = {"John"};
+    vector<string> tdirect = {"John"};
     vector<string> tgenre = {"Horror", "Comedy"};
     char tc = 'A';
     EXPECT_NO_THROW(recommend(tc, tgenre, tdirect, tactors));
