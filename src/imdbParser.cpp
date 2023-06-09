ca22a05c2df724ca80a02d3a8441e4ba2390cbd1
dzshao <danielzhshao@gmail.com>
2023-06-09 02:54:15 -0700

    Included headers in main.cpp. Fixes #53

--- src/imdbParser.cpp
+++ src/imdbParser.cpp
@@ -20,7 +20,7 @@ void imdbParser::scrapeGenres(const vector<string>&genreList) {
             continue;
         }
 
-        const int numMovies = 10;
+        const int numMovies = 49;
         scrapeMovies(htmlParser, listOfMovies, numMovies);
     }
 }
@@ -35,7 +35,7 @@ void imdbParser::scrapeMovies(stringstream& parser, vector<Movie>& movieList, in
         vector<Actor> actorList = findActorList(parser);
 
         movieList.push_back({movieTitle, releaseDate, movieGenres, movieRating, directorList, actorList});
-        MoviesDatabase::addMovie(movieList.at(i));
+        MoviesDatabase::addMovie(movieList.at(movieList.size() - 1));
     }
 }
 
